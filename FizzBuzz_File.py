44de535f821bb04048f0160d329c138bdb1b2ff0
yuxins0803-code <yuxins.0803@gmail.com>
2026-03-01 19:31:45 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def FizzBuzz(start, finish):
-    nums = np.arange(start, finish)
+    nums = np.arange(start, finish+1)
     out = nums.astype(object)
     mask3 = nums % 3 == 0
     mask5 = nums % 5 == 0
